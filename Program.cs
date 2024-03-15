df1b91f12ebe0cc1374f47af116cecc9bc341b03
justnazarko <justnazarko@gmail.com>
2024-03-15 09:13:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//variant 2
+using System;
+
+
+public enum DaysOfWeek
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+
+
+public class CalendarEvent
+{
+    public string EventName { get; set; }
+    public DaysOfWeek EventDay { get; set; }
+
+    
+    public CalendarEvent(string eventName, DaysOfWeek eventDay)
+    {
+        EventName = eventName;
+        EventDay = eventDay;
+    }
+
+    public void PrintEventDetails()
+    {
+        Console.WriteLine($" {EventName} will be on {EventDay}.");
+    }
+}
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalendarEvent myEvent = new CalendarEvent("Independence Day", DaysOfWeek.Saturday);
+        myEvent.PrintEventDetails();
+    }
+}
